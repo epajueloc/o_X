08205dc8417b17486b76855e7751f46a4692f914
Enrique Pajuelo <epajuelo@nd.edu>
2016-06-28 20:59:56 +0200

    Finished assignment 2b

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -10,6 +10,17 @@ class BoardViewController: UIViewController {
     @IBOutlet weak var newGameButton: UIButton!
     // Create additional IBOutlets here.
     
+    @IBOutlet weak var boardView: UIView!
+    
+    @IBAction func squareButton(sender: AnyObject) {
+        print(sender.tag)
+    }
+    
+    @IBAction func backButton(sender: AnyObject) {
+        print("Back")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
